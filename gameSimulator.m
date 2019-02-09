1c58945ffa8d6a706f4d7f516981ca525bc261d4
Tyler Giles <tjgiles@asu.edu>
2019-02-09 11:20:30 -0700

    Update gameSimulator.m

--- gameSimulator.m
+++ gameSimulator.m
@@ -2,17 +2,16 @@
 
 rounds = 12;
 
-TotalVuln = [];
-
 vuln = generateList(10,1);
 
-TotalVuln = vuln;
-
 for r = 1:rounds
+    r
     % players purchasing logic here
     % possibly may involve modifying the existing vuln list
     % this modified list will then be updated prior to starting
     % the next round
     vuln_u = updateList(vuln, .05, .05, 1);
-    %TotalVuln(
+    vuln_uCell = struct2cell(vuln_u);
+    % remove comment below to view output for eachround
+    %vuln_uCell
 end
