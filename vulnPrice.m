a92a7ac44064457e797aebf6bc3c5e04b275ed8e
Ryan Schmidt <skizzerz@skizzerz.net>
2019-02-09 11:50:32 -0700

    Fix bug with vulnPrice state

--- vulnPrice.m
+++ vulnPrice.m
@@ -1,5 +1,5 @@
 function [price, state] = vulnPrice(cvss, popularity, state)
-    rng(state.rng);
+    rng(state);
     price = cvss * popularity * (1 + randn / 10) * 5000;
-    state.rng = rng;
+    state = rng;
 end
