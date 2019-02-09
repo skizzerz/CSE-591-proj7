010b81ff8791be5bafa9e95a53043aa5c5439a63
Ryan Schmidt <skizzerz@skizzerz.net>
2019-02-09 16:20:50 -0700

    Add debug printouts

--- gameSimulator.m
+++ gameSimulator.m
@@ -6,11 +6,12 @@ budget = 1000000;
 avgVal = 0.2; % average value of a vulnerability
 %eMonth = budget / 12 * avgVal;
 eMonth = avgVal;
-[services, market, state] = initialize(num_svc);
+[services, market, state] = initialize(num_svc, 2);
 us = normalActor(num_svc, budget, rounds);
 opp = normalActor(num_svc, budget, rounds);
 
 for r = 1:rounds
+    fprintf('ROUND %d\n', r);
     bu = 0;
     bo = 0;
     % buy some vulns; we go first
@@ -19,18 +20,23 @@ for r = 1:rounds
     while isstruct(usPurchased) || isstruct(oppPurchased)
         if isstruct(usPurchased)
             bu = bu + 1;
+            fprintf('We purchased vuln with value/$ %.4f for $%.2f.\n', usValue, usPurchased.price);
             eFuture = calculateFutureValue(eMonth, rounds - r, us);
             [usPurchased, usValue, us, market] = purchase(us, eFuture, services, market,r);
         end
         
         if isstruct(oppPurchased)
             bo = bo + 1;
+            fprintf('Opponent purchased vuln with value/$ %.4f for $%.2f.\n', oppValue, oppPurchased.price);
             eFuture = calculateFutureValue(eMonth, rounds - r, opp);
             [oppPurchased, oppValue, opp, market] = purchase(opp, eFuture, services, market,r);
         end
     end
 
-    [r, bu, bo]
+    fprintf('---------------\n');
+    fprintf('We purchased $%.2f of vulns for total value %.4f.\n', us.spent(r), us.value(r));
+    fprintf('Opponent purchased $%.2f of vulns for total value %.4f.\n', opp.spent(r), opp.value(r));
+    fprintf('\n\n===============\n');
     
     % finally update market state and reset spent
     [market, state] = updateMarket(market, services, state);
